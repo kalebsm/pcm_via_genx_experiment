46cd600e0efa1af2c2b6c79200041773a601c182
sherryzuo <145484385+sherryzuo@users.noreply.github.com>
2025-03-04 18:11:57 -0500

    Updated all test case params
    - Starting capacities & generators from NYCA

--- SPCM/research_systems/NYISO_Case/Run.jl
+++ SPCM/research_systems/NYISO_Case/Run.jl
@@ -1,6 +1,6 @@
 
-    using GenX
-    using Gurobi
+        using GenX
+        using Gurobi
 
-    run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
-    
+        run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
+        
