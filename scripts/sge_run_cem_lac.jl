57c0cb00eef3e6c4b0030edd0186cff58140cb9b
kalebsm <56654036+kalebsm@users.noreply.github.com>
2025-07-31 16:29:47 -0400

    add file to run cem then lac

---
+++ scripts/sge_run_cem_lac.jl
@@ -0,0 +1,30 @@
+using Dates
+using Printf
+
+function run_CEM_and_LAC()
+    println(">>> Starting CEM case run at $(Dates.format(now(), "HH:MM:SS"))")
+    println(pwd())
+    cd("GenX.jl")
+    run(`julia -e '
+        using Pkg;
+        Pkg.activate(".");
+        Pkg.instantiate();
+        include("research_systems/run_CEM_cases.jl");
+        include("research_systems/cem_to_lac_capacity_update.jl")'
+    `)
+    cd("..")
+
+    println(">>> CEM run complete. Transferring data to LAC...")
+
+    cd("SPCM")
+    run(`julia -e '
+        using Pkg;
+        Pkg.activate(".");
+        Pkg.develop(path="src/scenario_generation/sequential_norta");
+        Pkg.instantiate();
+        include("research_systems/run_script.jl")'`)
+
+    println(">>> LAC case run complete at $(Dates.format(now(), "HH:MM:SS"))")
+end
+
+run_CEM_and_LAC()
